b2db6971f3d19f7711f93954dda3237e9cefc4c5
mcsmith01 <mcsmith01@alumni.amherst.edu>
2018-10-05 12:07:59 -0400

    Moved test code to main file

--- Homework4.java
+++ Homework4.java
@@ -42,5 +42,53 @@ public class Homework4 {
 
 	}
 
+	public static void main(String[] args) {
+		int exitCode = 0;
+
+		Homework4 hw4 = new Homework4("Hello, World!", 5);
+
+		if (hw4.multiply(2.5) == 12.25) {
+			System.out.println("Pass 1");
+		} else {
+			System.out.println("Fail 1");
+			exitCode += 1;
+		}
+
+		if (hw4.sumProduct(2, 2.5) == 20.25) {
+			System.out.println("Pass 2");
+		} else {
+			System.out.println("Fail 2");
+			exitCode += 1;
+		}
+
+		if (hw4.remainder(3.5) == 2) {
+			System.out.println("Pass 3");
+		} else {
+			System.out.println("Fail 3");
+			exitCode += 1;
+		}
+
+		if (hw4.getLength() == 13) {
+			System.out.println("Pass 4");
+		} else {
+			System.out.println("Fail 4");
+			exitCode += 1;
+		}
+
+		if (hw4.concat("Goodbye!").equals("Hello World! Goodbye!")) {
+			System.out.println("Pass 5");
+		} else {
+			System.out.println("Fail 5");
+			exitCode += 1;
+		}
+
+		if (exitCode == 0) {
+			System.out.println("All Pass");
+		} else {
+			System.out.println("Fail");
+		}
+
+		System.exit(exitCode);
+	}
 
 }
